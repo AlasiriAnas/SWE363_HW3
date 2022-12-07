24ce039be23031fd1438c67d52e664de16f39416
AlasiriAnas <almanaanas1@gmail.com>
2022-12-07 13:23:59 +0300

    writing the functions (empty)

---
+++ models/recipe_mode.js
@@ -0,0 +1,27 @@
+const  getAllRecipes = () =>{
+
+    return //array of recipes from the reccipes table 
+
+};
+
+
+const getRecipeDetail = (recipe_id) => {
+
+    return // one object that contains all the details of the recipe with the given id. 
+
+};
+
+
+const getComments = (recipe_id, comment) => {
+
+    return // list of comments of the the given recipe id  from the comments table 
+
+}; 
+
+
+const addComments = (recipe_id, comment) => {
+
+    //inserts in the comments table the comment given.
+    return // metadata about the inserted row.   
+
+}; 
---
+++ public/frontendJS.js
@@ -0,0 +1,18 @@
+const showComments = (recipe_id) => {
+
+   //sends an AJAX get request to “/recipes/:recipe_id/comments” and parse the received JSON to create the comments HTML and place them in the container mentioned in step 6.a.
+
+};
+
+
+const seeComments = (recipe_id) => {
+
+    //to handle when the user clicks on the “see comments” button. It unhides the comments section and call showComments.
+};
+
+
+const sendComments = (recipe_id) => {
+
+    //to handle when the user clicks on the “+” button of the comments form. The function should extract the author and the comment from the form fields. Then it sends an AJAX POST request to  “/recipes/:recipe_id/comments” sending a JSON encoded comment object. Then it calls showComments to update the page.
+
+};
